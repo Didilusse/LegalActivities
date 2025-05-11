f8e21ebe9801eff3bead007904dcae424638514e
Adil Rahmani <didilusse@gmail.com>
2025-05-11 14:11:34 -0400

    Initial Commit

---
+++ LegalActivities/LegalActivitiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  LegalActivitiesApp.swift
+//  LegalActivities
+//
+//  Created by Adil Rahmani on 5/11/25.
+//
+
+import SwiftUI
+
+@main
+struct LegalActivitiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
